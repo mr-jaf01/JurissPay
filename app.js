09ff84f2ff675b24a5125f53e3624e4c9afff254
mrjaf <sheikjafar32@gmail.com>
2020-09-14 13:18:46 +0100

    he

--- app.js
+++ app.js
@@ -25,16 +25,23 @@ const transact = require('./tools/transact');
 
 const app = express();
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+
 const dburl  = process.env.MONGODB_URI  || 'mongodb://localhost:27017/wallet';
-//-------db connecttion---/////
+//--------------db connecttion---------------------------//
 mongoose.connect(dburl, {useNewUrlParser: true, useUnifiedTopology: true })
         .then((result)=>{
-            console.log('connected to Database');
+            app.listen(process.env.PORT, ()=>{
+                console.log('JurissPay Server Started');
+                console.log('connected to Database');
+            });            
         }).catch((err)=>{
             console.log('Cannot Connect to DBserver');
 });
 mongoose.set('useFindAndModify', false);
-//--------------------//----------------//
+//--------------db connection end hereee----------------//
+
+
 
 ///-----------app middleware------------// 
 app.use(session({
@@ -53,7 +60,6 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(layouts);
-
 ///------------------------------//
 
 
@@ -325,11 +331,3 @@ app.get('/services/wallet-profile', (req,res)=>{
 
 
 
-
-
-
-///------------------------------///
-///------------------Port Listen----------////
-app.listen(process.env.PORT, ()=>{
-    console.log('JurissPay Server Started');
-})
